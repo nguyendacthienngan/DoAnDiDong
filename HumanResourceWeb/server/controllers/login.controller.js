4ebf464416055bf0a985e3da89813d357ec3544f
nguyendacthienngan <nguyendacthienngan@gmail.com>
2020-12-02 02:02:56 +0700

    Move server backend to new repo

--- HumanResourceWeb/server/controllers/login.controller.js
+++
@@ -1,7 +0,0 @@
-
-
-module.exports.postLogin = (req, res, next) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    res.send('POST request to the homepage');
-}
