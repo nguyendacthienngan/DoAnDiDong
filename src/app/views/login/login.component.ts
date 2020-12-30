12e08b62d05f1b285fdf8f9f7da358c3eedfb0a5
nguyendacthienngan <nguyendacthienngan@gmail.com>
2020-12-31 05:28:22 +0700

    Working on login

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AccountService } from '../../shared/services/account/account.service'
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, FormArray, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,20 +9,35 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['login.component.css']
 })
 export class LoginComponent { 
-  // username: string;
-  // password: string;
-  // datas: any;
-  // constructor(
-  //   private accountService : AccountService,
-  //   private router : Router
-  // ) { }
+  username: string;
+  password: string;
+  datas: any;
+  // login_form: FormGroup;
+  constructor(
+    private accountService : AccountService,
+    private router : Router,
+    //private builder: FormBuilder,
+  ) { }
 
-  // ngOnInit(): void {
+  ngOnInit(): void {
+  //   this.login_form = this.builder.group({
+  //     username: new FormControl(null, []),
+  //     password: new FormControl(null, []),
+  // });
+
+  //   // 
+  }
+  // login() {
+  //   this.username = this.login_form.controls['username'].value;
+  //   this.password = this.login_form.controls['password'].value;
   //   this.accountService.login(this.username, this.password).subscribe((res:any)=>{
-  //     this.datas = res;
-  //     let canPath = '/home';
-  //     this.router.navigateByUrl(canPath);
-  //   })
+  //       this.datas = res;
+  //       let canPath = '/home';
+  //       this.router.navigateByUrl(canPath);
+  //     })
   // }
-  
+
+  login() {
+    this.router.navigateByUrl('/home');
+  }
 }
